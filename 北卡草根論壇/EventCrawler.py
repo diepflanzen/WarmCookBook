7162d73ee0ea38a905db67a4f5a98e03dfc3d62d
JiangengChiou <diepflanzen@gmail.com>
2017-07-03 11:51:04 -0400

    crawled three pages

--- 北卡草根論壇/EventCrawler.py
+++ 北卡草根論壇/EventCrawler.py
@@ -6,13 +6,13 @@
 import facebook
 
 # 用 https://findmyfbid.com 找出Event列表的id
-ID = '349696661821984'
-groupname = u'紐約哲學星期五'
+ID = '1615086715385428'
+groupname = u'北卡草根論壇'
 csvname = groupname + '.csv'
 txtname = groupname + '.txt'
 
 # 爬出這個ID的facebook page 的 event 資料（access token 這樣取得： https://developers.facebook.com/tools/explorer/?method=GET&path=me%3Ffields%3Did%2Cname&version=v2.9）
-graph = facebook.GraphAPI(access_token='EAACEdEose0cBAIrPxYHvjPeJwAiNDzIZC7sNKGrsFmLeglUlzmGqMYGTwruhmcwTudMmYIOirStyHZBx8fovSyiN1UiFfmAZCoO8ZBhWZC06ZC3Ab7M89MWysIMoPCY8qbxvyuZCTWbAT8IEKucoaogcZBKXBtZANIw3LCOQOj15yRD8BKi7IikUrOZAvB8PYQJL8ZD', version='2.7')
+graph = facebook.GraphAPI(access_token='EAACEdEose0cBAEgfaTALjFZBujrPfkiWPnL2ZA7LuFdfgJtdsRKC389TnNBpsXzK79HuKbeaxX5kZClqqdQ7tajJQyij34N30ucIMYa9ZCVUgftgrbZCdpXPTGXOqR4pzrJihZApOtcQ9N49VBMw6WZCBXZBRrGxHJDEG0cdlEs1FLaL0ljM4PeTtkQB6p48WYcZD', version='2.7')
 a = graph.get_object( ID + '/events?limit=50')
 
 
@@ -36,14 +36,18 @@ with open(csvname, 'w') as f:
 
 
         # 找出"description"的內容
-        description_content = event['description']
-        
-        
-        # 找到符合'【主講人】.....【 '的字串
-        speaker_start = description_content.find(u'【主講人】')
-        speaker_end = description_content.find(u'\n', speaker_start+1)
-        speaker = description_content[speaker_start:speaker_end]
-        
+        speaker = ''
+        try:
+            description_content = event['description']
+           
+                    
+            # 找到符合'講者..... '的字串
+            speaker_start = description_content.find(u'講者')
+            speaker_end = description_content.find(u'\n', speaker_start+1)
+            speaker = description_content[speaker_start:speaker_end]
+        except:
+            speaker = ''
+
         
         # 把剛剛那行用逗點分隔，寫進剛開的spreadsheet檔裡面。
         to_print = ', '.join([issue_content, speaker, groupname, time])
@@ -69,7 +73,10 @@ with open(txtname, 'w') as f:
 
 
         # 找出"description"的內容
-        description_content = event['description']
+        try:
+            description_content = event['description']
+        except:
+            description_content = ''
                         
         # 把題目，時間，description，寫進剛開的文字檔裡面。
         to_print = '%s \n %s \n---\n %s \n===\n' %(issue_content, time, description_content)
